be993dd688b0a3698d0aa2eda914045a337df1cf
Concept Vi <v.i@conceptv.com.au>
2026-06-01 15:58:22 +1000

    Revert "[self-apply] add node-type 'mirrortext'"
    This reverts commit a1063b5d721f93e6e36c97881def7e7535ce8b2a.

--- nodes/mirrortext.py
+++
@@ -1,8 +0,0 @@
-ORIGIN = 'system'  # brain-written (self-grown) — provenance layer
-VERSION='1'
-KIND='process'
-PORTS_IN={'text':'Text'}
-PORTS_OUT={'text':'Text'}
-
-def run(inputs, config):
-    return str(inputs.get('text', ''))[::-1]
